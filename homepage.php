59f60d1f76964a42427dbeff5bd7bf23533d3fb5
aizaanwar <aiza.anwar@gmail.com>
2017-04-26 11:16:33 +0500

    homepage.php is added

---
+++ homepage.php
@@ -0,0 +1,138 @@
+<html>
+<head>
+<title>Sprinkles</title>
+<meta charset="utf-8">
+   
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-heme.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+  <style>
+  input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 10px;
+    font-size: 16px;
+    background-color: white;
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 30px;
+    
+    transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+    width: 100%;
+}
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  body  {background-image:url("b.jpg"); background-color:#ccffb3;font-family: "new century schoolbook";}
+  a span{ color :white}
+.h1 {color:brown}
+
+
+  </style>
+
+</head>
+
+<body>
+<div class="container"  style="font-size:22">
+<div class="row"style="border: 1px solid black" height="5px">
+  <div class="col-md-4"><img src="sprinkles.png"/ align="left" height="85px" width="250px" style="border:2px solid orange" ></div>
+  <div class="col-md-8"><a href="https://www.facebook.com/"><img src="facebook.png" align=right height="35px" width="35px"/><a href="https://twitter.com/login"><img src="Twitter_Logo.png" align=right height="35px" width="35px"/><a href="https://www.instagram.com/?hl=en"><img src="insta.png" align=right height="35px" width="35px"/></div>
+</div>
+
+<div id="1"class="row">
+  <div class="col-md-3"style="text-align:center;background-color:#FE8426"><a href="file:///C:/Users/home/Desktop/Website%20phase1/home%20page.html#"><span>Home</span></a></div>
+  <div class="col-md-3"style="text-align:center;background-color:#34CD23"><a href="file:///C:/Users/home/Desktop/Website%20phase1/About%20Us.html#"target="_blank"><span>About Us</span></a></div>
+  <div class="col-md-3"style="text-align:center;background-color:#F93E3E"><a href="file:///C:/Users/home/Desktop/Website%20phase1/Products.html"target="_blank"><span>Products</span></a></div>
+  <div class="col-md-3"style="text-align:center;background-color:#071783"><a href="file:///C:/Users/home/Desktop/Website%20phase1/Contact%20Us.html"target="_blank"><span>Contact Us</span></a></div>
+</div>
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="SLIDER1.png" alt="welcome" width="460" height="200">
+      </div>
+
+      <div class="item">
+        <img src="slider4.jpg" alt="Chania" width="460" height="200">
+      </div>
+    
+      <div class="item">
+        <img src="slider2.jpg" alt="cake" width="460" height="200">
+      </div>
+
+      <div class="item">
+        <img src="slider3.jpg" alt="icecream" width="460" height="200">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<div class="h1">
+<h1><b>~SPRINKLES~</b></h1>
+<h3><b>Latest Products</b></h3>
+
+<div class="row"style="align:center;text-align:center;">
+<div class="col-md-4"><img src="kulfi.jpg" height="150px" width="150px"align="center"/><figcaption>Badami Khulfi</figcaption></div>
+<div class="col-md-4"><img src="rasgulla.jpg"height="150px" width="150px"align="center"/><figcaption>Rasgullay</figcaption></div>
+<div class="col-md-4"><img src="ghulab.jpg"height="150px" width="150px" align="center"/><figcaption>Ghulab Jaman</figcaption></div>
+</div>
+
+<br>Please fill in the form for the products you wish to have. 
+<button style="background-color:#e65c00">
+<a href="file:///C:/Users/home/Downloads/samina.html"target="_blank"/><span>Order form</span></a></button>
+</div>
+</div>
+<br><br><br>  
+<!--Footer-->
+
+<footer style="background-color:#e65c00;height:45%; width:100%;">
+<br>
+<center><h3>Search from site:</h3>
+<form >
+  <input type="text" name="search" placeholder="Search...">
+</form>
+
+<h3>Futher Detail</h3>
+  <p>E-mail:sprinkles123@gmail.com</p>
+  <p>Contact:03XX-XXXXXXX</p>
+  <p>Address:XYZ Raiwand Road Lhr</p>
+</footer>
+
+</body>
+</html>
